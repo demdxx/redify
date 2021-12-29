271da2ccab331e0525d6904bf2c26fb9ac837bd1
Dmitry Ponomarev <demdxx@gmail.com>
2021-12-29 19:55:48 +0100

    Up SQL stmt test

--- internal/storage/sql/sql_types_test.go
+++ internal/storage/sql/sql_types_test.go
@@ -8,22 +8,15 @@ import (
 
 func TestWhereStmt(t *testing.T) {
 	wr := WhereStmt{`col1`: `val1`, `col2`: `val2`}
-	wrs := wr.Where(`"`, "ext")
-	res := false ||
-		wrs == ` WHERE "col1"=val1 AND "col2"=val2 AND ext` ||
-		wrs == ` WHERE "col2"=val2 AND "col1"=val1 AND ext`
-	assert.True(t, res)
+	assert.Contains(t, []string{
+		` WHERE "col1"=val1 AND "col2"=val2 AND ext`,
+		` WHERE "col2"=val2 AND "col1"=val1 AND ext`,
+	}, wr.Where(`"`, "ext"))
 }
 
 func TestSQLData(t *testing.T) {
 	wr := DataFields{`col1`: `val1`, `col2`: `val2`}
-	assert.True(t, false ||
-		wr.Columns(`"`) == `"col1", "col2"` ||
-		wr.Columns(`"`) == `"col2", "col1"`)
-	assert.True(t, false ||
-		wr.Values() == `val1, val2` ||
-		wr.Values() == `val2, val1`)
-	assert.True(t, false ||
-		wr.SetValues(`"`) == `"col1"=val1, "col2"=val2` ||
-		wr.SetValues(`"`) == `"col2"=val2, "col1"=val1`)
+	assert.Contains(t, []string{`"col1", "col2"`, `"col2", "col1"`}, wr.Columns(`"`))
+	assert.Contains(t, []string{`val1, val2`, `val2, val1`}, wr.Values())
+	assert.Contains(t, []string{`"col1"=val1, "col2"=val2`, `"col2"=val2, "col1"=val1`}, wr.SetValues(`"`))
 }
